344aeb3ccf08bd56661709c5a1d5c00624002484
ander508 <oluchukwu1.offor@gmail.com>
2025-11-29 20:34:26 +0100

    Stickiness and validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/index.js")
 const accountModel = require("../models/account-model.js")
+const bcrypt = require("bcryptjs")
 
 async function buildLogin(req, res, next) {
     let nav = await utilities.getNav()
@@ -7,6 +8,7 @@ async function buildLogin(req, res, next) {
     res.render("account/login", {
         title: "Login",
         nav,
+        errors:null
     })
 }
 
@@ -15,7 +17,7 @@ async function buildRegister(req, res, next){
     let nav = await utilities.getNav()
       // req.flash("notice", "This is a flash message.")
       res.render("account/register", {
-        title: "Register",
+        title: "Register Here",
         nav,
         errors: null
       })
@@ -28,13 +30,32 @@ async function buildRegister(req, res, next){
 * *************************************** */
 async function registerAccount(req, res) {
   let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+  const { account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+    }
+    = req.body
+
+  //Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
 
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -54,6 +75,10 @@ async function registerAccount(req, res) {
     })
   }
 }
+
+
+
+
 
 
 module.exports = { buildLogin, buildRegister, registerAccount }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,11 +5,25 @@ const accountController = require("../controllers/accountController.js")
 const regValidate = require("../utilities/account-validation.js")
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+
 // Process the registration data
 router.post("/register",
     regValidate.registrationRules(),
     regValidate.checkRegData,
     utilities.handleErrors(accountController.registerAccount))
+
+
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+ (req, res) => {
+    res.status(200).send('login process')
+  }
+  
+)
 
 
 module.exports = router;
