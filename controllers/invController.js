929951238413f38b550a9ff08f3c549a7be18983
ander508 <oluchukwu1.offor@gmail.com>
2025-11-30 22:03:48 +0100

    Week04 Assignment completed

--- controllers/invController.js
+++ controllers/invController.js
@@ -48,4 +48,120 @@ invCont.throwError = async function (req, res) {
 }
 
 
+invCont.buildManagementView = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const classificationSelect = await utilities.buildClassificationList()
+  res.render("./inventory/management", {
+    title: "Management",
+    nav,
+    errors: null,
+    classificationSelect,
+  })
+}
+
+
+invCont.newClassificationView = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null,
+  })
+}
+
+
+
+invCont.addClassification = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const { classification_name } = req.body
+
+ 
+  const insertResult = await invModel.addClassification(classification_name)
+
+  if (insertResult) {
+    nav = await utilities.getNav()
+    req.flash("message success", `The ${insertResult.classification_name} classification was successfully added.`)
+    res.status(201).render("./inventory/management", {
+      title: "Management",
+      nav,
+      errors: null,
+      
+    })
+  } else {
+    req.flash("message warning", "Sorry, the insert failed.")
+    res.status(501).render("./inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      errors: null,
+    })
+  }
+}
+
+
+
+invCont.newInventoryView = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const classificationSelect = await utilities.buildClassificationList()
+  res.render("./inventory/add-inventory", {
+    title: "Add New Inventory",
+    nav,
+    classificationSelect: classificationSelect,
+    errors: null,
+  })
+}
+
+
+invCont.addInventory = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const {
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id,
+  } = req.body
+  const insertResult = await invModel.addInventory(
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id
+  )
+
+  if (insertResult) {
+    const itemName = insertResult.inv_make + " " + insertResult.inv_model
+    const classificationSelect = await utilities.buildClassificationList()
+    req.flash("message success", `The ${itemName} was successfully added.`)
+    res.status(201).render("./inventory/management", {
+      title: "Inventory Management",
+      nav,
+      errors: null,
+      classificationSelect,
+    })
+  } else {
+    const classificationSelect = await utilities.buildClassificationList()
+    req.flash("message warning", "Sorry, the insert failed.")
+    res.status(501).render("./inventory/add-inventory", {
+      title: "Add New Inventory",
+      nav,
+      classificationSelect: classificationSelect,
+      errors: null,
+    })
+  }
+}
+
+
+
+
+
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,9 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/index")
+const invChecks  = require("../utilities/inventory-validation")
+
+
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
@@ -19,6 +22,42 @@ router.get(
     utilities.handleErrors(invController.throwError))
 
 
+router.get(
+  "/",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.buildManagementView)
+)
+
+router.get(
+  "/newClassification",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newClassificationView)
+)
+
+
+
+router.post(
+  "/addClassification",
+  //utilities.checkAccountType,
+  invChecks.classificationRule(),
+  invChecks.checkClassificationData,
+  utilities.handleErrors(invController.addClassification)
+)
+
+
+router.get(
+  "/newVehicle",
+  //utilities.checkAccountType,
+  utilities.handleErrors(invController.newInventoryView)
+)
+
+router.post(
+  "/addInventory",
+  //utilities.checkAccountType,
+  invChecks.newInventoryRules(),
+  invChecks.checkInventoryData,
+  utilities.handleErrors(invController.addInventory)
+)
 
 
 module.exports = router;
